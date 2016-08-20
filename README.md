5db94c765bb7b199cfb0de66b49bc73edc583dba
Nic Myers <nicmyers@w3designs.net>
2016-08-20 10:07:18 -0700

    Removing an excessive amount of console.log's Also updating the readme with more info about future plans.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 #Elevator Saga Attept
 http://play.elevatorsaga.com/
 
+My attempt at Elevator Saga seeks to recreate how I think elevators should work. The basic idea is that the elevators should go from bottom to top, picking up passengers if they are not full, and then return when they reach their calculated top point. The elevator indicator lights are working, though in some of the challenges disabling them allows the code to pass, as then the patrons are not as picky about which elevator they choose. There are improvements I can make, which are listed in the ideas section below.
+
 ## Challenges Completed
 The code in elevator.js has passed all of the levels listed below at least once. Though currently it won't pass 100% of the time. Sometimes there are events that the current queue system is not optimised for.
 
@@ -24,3 +26,25 @@ The code in elevator.js has passed all of the levels listed below at least once.
 16. Pass
 17. Pass
 18. Not Passing: Not currently optimised for low wait times and usually someone gets stuck on a floor
+
+
+## Ideas & Improvements
+1. The current versions scheduling of requested stops is not ideal. I'm thinking a priority system based upon waiting time per floor could be useful.
+2. Ideally, when an elevator is idle, it will respond for pickup on a floor that it is closest to.
+3. Currently, all elevators respond independently. Perhaps a system that chooses the closest idle elevator and then pushes a requested floor into its respective queue would be the best. This would solve the bug of having multiple idle elevators responding to a single call of the elevator. 
+
+## Configuration
+There are a few configuration options available. As the improvements are introduced, the need to change these values will be reduced and they may be removed.
+
+**Maxload** 
+These control the max load of the elevator before they will go directly to the destination without picking up people on the way.
+'var maxLoad = 0.5;' (Default)
+'var maxLargeLoad = 0.5;' (Default)
+
+**Elevator Direction Lights** 
+Enable or disable the elevator direction lights. This settings sometimes allows challenges pass.
+'var directionLights = true;' (Default)
+
+**Enable Logging**
+Set to true to enable console logging of elevator activity.
+'var logging = false;' (Default)
--- elevator.js
+++ elevator.js
@@ -1,4 +1,3 @@
-
 {
   init: function(elevators, floors) {
 
@@ -8,15 +7,13 @@
     var maxLargeLoad = 0.5;
 
     // Turn on Elevator lights
-    var directionLights = false;
+    var directionLights = true;
 
-    // Uncomment to disable logging
-    // console.log = function() {}
-    var logging = true;
+    // Enable or disable console logging of elevator and queue activity
+    var logging = false;
 
     //// Defaults
-    var floorCount = floors.length-1;
-    console.log("Number of Floors: " + floorCount);
+    var floorCount = floors.length - 1;
 
     // Queues for the up and down passengers. 
     var upQueue = [];
@@ -37,19 +34,18 @@
       var floorButtonsQueue = [];
 
 
-      ///////////////////////////////////////////////////////////
       // Set the next floor that the elevator should go to.
       function nextFloor() {
-
+        // Update the unified up down queue
         updatePickupQueue();
 
         if (elevator.travelDirection == 'up' && floorButtonsQueue.length > 0) {
 
-          // Find people on other floors in the direction we are going
+          // Find people on other floors in the direction we are going.
           pickupHitchkikers();
-          // Make sure the Queue is properly sorted
+          // Make sure the Queue is properly sorted.
           sortQueue();
-          // Make sure we are not trying to go to the place we are already at.
+          // Clean the current floor from the queue.
           cleanGlobalQueues();
 
           if (floorButtonsQueue.length > 0) {
@@ -60,11 +56,11 @@
 
         } else if (elevator.travelDirection == 'down' && floorButtonsQueue.length > 0) {
 
-          // Find people on other floors
+          // Find people on other floors in the direction we are going.
           pickupHitchkikers();
-          // Make sure the Queue is properly sorted
+          // Make sure the Queue is properly sorted.
           sortQueue();
-          // Make sure we are not trying to go to the place we are already at.
+          // Clean the current floor from the queue.
           cleanGlobalQueues();
 
           if (floorButtonsQueue.length > 0) {
@@ -74,60 +70,45 @@
           }
 
         } else if (downQueue.length > 0) {
-          //console.log("Grab someone who wants to go down");
           elevator.goToFloor(downQueue[0], checkLoad());
-
         } else if (upQueue.length > 0) {
-          //console.log("Grab someone who wants to go up");
           elevator.goToFloor(upQueue[0], checkLoad());
-
         } else if (requestedQueue.length > 0) {
-          //console.log("No pushed anything inside, go to a requested floor : " + requestedQueue);
           elevator.goToFloor(requestedQueue[0], checkLoad());
-
         } else {
           elevator.goToFloor(0);
-          
         }
       }
 
 
-      ///////////////////////////////////////////////////////////
       // Sort the floor buttons queue
       function sortQueue() {
         if (elevator.travelDirection == "up") {
           floorButtonsQueue = floorButtonsQueue.sort(function (a, b) { return a - b });
-          //console.log("Elevator " + index + " Next Floors (Up, Sorted): " + floorButtonsQueue);
         } else {
           floorButtonsQueue = floorButtonsQueue.sort(function (a, b) { return b - a });
-          //console.log("Elevator " + index + " Next Floors (Down, Sorted): " + floorButtonsQueue);
-        }
-      }
-
-      function logStatus(){
-        if(logging) {
-          console.log("Elevator " + index + " | Queue: " + floorButtonsQueue + " | Current Floor: "+ elevator.currentFloor() + "| Direction: " + elevator.travelDirection + " | Current Load: " + elevator.loadFactor() + " | Capacity: " + elevator.maxPassengerCount());
         }
       }
 
 
-      ///////////////////////////////////////////////////////////
+      // Basic logging of individual elevator activity.
+      function logStatus() {
+        if (logging) {
+          console.log("Elevator " + index + " | Queue: " + floorButtonsQueue + " | Current Floor: " + elevator.currentFloor() + " | Direction: " + elevator.travelDirection + " | Current Load: " + elevator.loadFactor() + " | Capacity: " + elevator.maxPassengerCount());
+        }
+      }
+
+
       // Check Load - See if both large and small elevators are full
       function checkLoad() {
-        //console.log("Elevator " + index + " Current Load: " + elevator.loadFactor() + " | Capacity: " + elevator.maxPassengerCount());
-
         if (elevator.loadFactor() < maxLoad && elevator.maxPassengerCount() <= 5) {
           var elevatorFull = false;
-          //console.log("Elevator " + index + "(Small) is not full");
         } else if (elevator.loadFactor() < maxLargeLoad && elevator.maxPassengerCount() > 5) {
           var elevatorFull = false;
-          //console.log("Elevator " + index + "(Large) is not full");
         } else if (elevator.loadFactor() > maxLoad && elevator.maxPassengerCount() <= 5) {
           var elevatorFull = true;
-          //console.log("Elevator " + index + "(Small) is full");
         } else if (elevator.loadFactor() > maxLargeLoad && elevator.maxPassengerCount() >= 5) {
           var elevatorFull = true;
-          //console.log("Elevator " + index + "(Large) is full");
         } else {
           var elevatorFull = false;
         }
@@ -135,77 +116,52 @@
       };
 
 
-      ///////////////////////////////////////////////////////////
       // Find the next passengers to pick up, if idle
       function findNextPickupArray() {
         requestedQueue = _.union(upQueue, downQueue);
-        //console.log("Combined Pickup Queue:" + requestedQueue);
-
-        //requestedQueue = _.uniq(requestedQueue);
-        //console.log("requested queue after:" + requestedQueue); 
-
-        //console.log('Idle, so adding a destination from the requsted queue: ' + requestedQueue);
         return (requestedQueue[0]);
       }
 
 
-      ///////////////////////////////////////////////////////////
       // Find stops on the way and add them to the queue, if not full 
       function pickupHitchkikers() {
-
         if (checkLoad() == false) {
           if (elevator.travelDirection == 'up' && upQueue.length > 0) {
             var floorsAbove = _.filter(upQueue, function (n) { return n > elevator.currentFloor() });
-            //console.log("Elevator " + index + " has requested Floors Above: " + floorsAbove);
-            console.log("Elevator " + index + " New Global Queue before Hitchhikers: " + floorButtonsQueue);
             floorButtonsQueue = _.union(floorsAbove, floorButtonsQueue);
             sortQueue();
-            console.log("Elevator " + index + " New Global Queue with Hitchhikers: " + floorButtonsQueue);
             upQueue = _.difference(floorButtonsQueue, upQueue);
-            //console.log("Queue of upwards requests: " + _.difference(floorButtonsQueue, upQueue));
           }
 
           else if (elevator.travelDirection == 'down' && downQueue.length > 0) {
             var floorsBelow = _.filter(downQueue, function (n) { return n < elevator.currentFloor() });
-            //console.log("Elevator " + index + " has requested Floors Above: " + floorsBelow);
-            console.log("Elevator " + index + " New Global Queue before Hitchhikers: " + floorButtonsQueue);
             floorButtonsQueue = _.union(floorsBelow, floorButtonsQueue);
             sortQueue();
-            console.log("Elevator " + index + " New Global Queue with Hitchhikers: " + floorButtonsQueue);
             downQueue = _.difference(floorButtonsQueue, downQueue);
-           //console.log("Queue of downwards requests: " + _.difference(floorButtonsQueue, downQueue));
           }
-        } else {
-          console.log("Elevator " + index + " is full, skipping Hitchhikers");
         }
       }
 
 
-      //////////////////////////////////////////////////////////
       // Max and Min Floors 
       function getMaxFloor(numArray) {
         if (numArray.length > 0) {
-          //console.log("Elevator " + index + " Max Floor: " + _.max(numArray));
           return _.max(numArray);
         } else {
-          //console.log("Elevator " + index + " Array Empty Setting Max Floor: " + floorCount);
           return floorCount;
         }
       }
 
       function getMinFloor(numArray) {
         if (numArray.length > 0) {
-          //console.log("Elevator " + index + " Min Floor: " + _.min(numArray));
           return _.min(numArray);
         } else {
-          //console.log("Elevator " + index + " Array Empty Setting Min Floor: 0 ");
           return 0;
         }
       }
 
 
-      ///////////////////////////////////////////////////////////
-      // This sets the up or down travel direction
+      // This sets the travel direction: up or down
       function setTravelDirection() {
         if (elevator.currentFloor() <= getMinFloor(upQueue)) {
           elevator.travelDirection = "up";
@@ -214,7 +170,6 @@
             elevator.goingDownIndicator(false);
             elevator.goingUpIndicator(true);
           }
-          //console.log("Elevator " + index + " Changed Direction: " + elevator.travelDirection);
         }
         else if (elevator.currentFloor() >= getMaxFloor(downQueue)) {
           elevator.travelDirection = "down";
@@ -222,7 +177,6 @@
             elevator.goingDownIndicator(true);
             elevator.goingUpIndicator(false);
           }
-          //console.log("Elevator " + index + " Changed Direction: " + elevator.travelDirection);
         }
         else {
           elevator.travelDirection = "down";
@@ -234,15 +188,11 @@
       };
 
 
-      ///////////////////////////////////////////////////////////
       // Clear the Queue when the elevator is at the requested floor
       function cleanGlobalQueues() {
-
         if (floorButtonsQueue.length > 0) {
           if (elevator.currentFloor() == floorButtonsQueue[0]) {
-            //console.log("Elevator " + index + " Removing Floor from Global Queue: " + floorButtonsQueue[0]);
             floorButtonsQueue.shift();
-            //console.log("Elevator " + index + " New Global Queue: " + floorButtonsQueue);
             return floorButtonsQueue;
           } else {
             return floorButtonsQueue;
@@ -251,12 +201,10 @@
 
         // Clear the current floor from the downQueue, if elevator is at it
         if (elevator.currentFloor() == downQueue[0]) {
-          //console.log("Removing " + downQueue[0] + " from downQueue, Remaining Requested:" + downQueue);
           downQueue.shift();
         }
         // Remove the current floor from the UpQueue, if elevator is at it
         if (elevator.currentFloor() == upQueue[0]) {
-          //console.log("Removing " + upQueue[0] + " from upQueue, Remaining Requested:" + upQueue);
           upQueue.shift();
         }
       }
@@ -265,9 +213,7 @@
       ///////////////////////////////////////////////////////////
       // Whenever the elevator is idle (has no more queued destinations) ...
       elevator.on("idle", function () {
-        //console.log("Elevator " + index + " is Idle On Floor: " + elevator.currentFloor());
-
-        // Output a log of what the elevator is doing
+        // Display logging if enabled
         logStatus();
 
         // Find people on other floors in the direction we are going
@@ -278,83 +224,71 @@
 
         // Set the next floor
         nextFloor();
-
       });
 
 
       ///////////////////////////////////////////////////////////
       // What to do When the elevator is stopped at a floor
       elevator.on("stopped_at_floor", function (floorNum) {
-        //console.log("Elevator " + index + " Stoped at Floor: " + floorNum);
-
+        // Display logging if enabled
         logStatus();
 
-        // Find people on other floors in the direction we are going
-        // pickupHitchkikers();
+        // Update the unified up down queue
         updatePickupQueue();
 
         // Check the load of the elevators
         checkLoad();
 
-        // Cleanup the Global Queue
+        // Cleanup the global queues
         cleanGlobalQueues();
 
-        // Sort the global Queue
+        // Sort the elevator queue
         sortQueue()
 
         // Set the direction of travel
         setTravelDirection();
-
       });
 
 
-      ///////////////////////////////////////////////////////////
       // What to do When the elevator is moving
-      elevator.on("passing_floor", function(floorNum, direction) { 
+      elevator.on("passing_floor", function (floorNum, direction) {
+        // Find people on other floors in the direction we are going
         pickupHitchkikers();
-      
-     });
+      });
 
-
-      ///////////////////////////////////////////////////////////
       // When the button is pressed in the elevator
       elevator.on("floor_button_pressed", function (floorNum) {
         floorButtonsQueue.push(floorNum);
         sortQueue();
-
-        //console.log("Elevator " + index + " Inside Button Pressed: " + floorButtonsQueue);
       });
 
     } // End Main Elevator
 
 
-    ///////////////////////////////////////////////////////////
     //  Update the next passengers to pick up queue
     function updatePickupQueue() {
       requestedQueue = _.union(upQueue, downQueue);
-      console.log("Requested: " + requestedQueue + " | Up: " + upQueue + " | Down: " + downQueue);
+      if (logging) {
+        console.log("Requested: " + requestedQueue + " | Up: " + upQueue + " | Down: " + downQueue);
+      }
     }
 
 
     // Find who is waiting on each floor for the elevator
     function floorPanels(floor) {
-
       floor.on("up_button_pressed", function () {
         upQueue.push(floor.level);
         updatePickupQueue();
-        //console.log("Request (Up) at Floor: " + upQueue);
       });
 
       floor.on("down_button_pressed", function () {
         downQueue.push(floor.level);
-        //downQueue = _.uniq(downQueue);
         updatePickupQueue();
-        //console.log("Request (Down) at Floor: " + downQueue);
       });
     }
 
-
-    // --------- Initalize the Elevator -----------------------
+    ///////////////////////////////////////////////////////////
+    // --------- Power up the Elevator and Floor Panels -----------------------
 
     // Get all the elevators moving    
     elevators.forEach(powerOnElevator);
